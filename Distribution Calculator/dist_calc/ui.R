d7163fceca1bbd8d256cfaea5d17a881e7e2db1e
Davood Astaraky <Astaraky@telfer.uottawa.ca>
2015-09-29 23:03:59 -0400

    update

--- Distribution Calculator/dist_calc/ui.R
+++ Distribution Calculator/dist_calc/ui.R
@@ -1,8 +1,8 @@
 library(shiny)
 
 shinyUI(pageWithSidebar(
-  
-  headerPanel("Distribution Calculator"),
+        
+        headerPanel(h3("ADM2303B&C - Distribution Calculator")),
   
   sidebarPanel(
     #radio button or dropdown?
@@ -45,8 +45,8 @@ shinyUI(pageWithSidebar(
     strong("Resources and References:"),   
     
     helpText("Created using" , a(href="http://www.r-project.org/", target="_blank", "R."), "Hosted by" , a(href="http://www.rstudio.com/", target="_blank", "RStudio.")),
-    helpText("Source code available on" , a(href="??", target="_blank", "GitHub.")),
-    helpText("Codes are adopted based on the work of Dr. Mine Cetinkaya-Rundel, Duke university,in collaboration with "),  a(href="https://stat.duke.edu/people/brittany-cohen", target="_blank", "Brittany Cohen.")),
+    helpText("Source code available on" , a(href="https://github.com/Davood-Astaraky/ADM2303/tree/gh-pages/Distribution%20Calculator/dist_calc", target="_blank", "GitHub.")),
+    helpText("Codes are adopted based on the work of" , a(href="https://github.com/mine-cetinkaya-rundel", target="_blank", "Dr. Mine Cetinkaya-Rundel"), ", Duke university, in collaboration with",  a(href="https://stat.duke.edu/people/brittany-cohen", target="_blank", "Brittany Cohen."))),
   
   
   
